2b4cf68fee0574c1f30bbcce6fe1552f41926c10
tzitziris <stzitzires@gmail.com>
2024-10-21 21:09:29 +0300

    small changes

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,13 +1,32 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';  // Import Link from react-router-dom
+// import '../assets/Navbar.css'; 
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar"> 
+//       <h1 className="title">Fight News</h1>
+//       <ul className="navLinks"> 
+//         <li><Link to="/home" className="link">Home</Link></li> 
+//         <li><Link to="/news" className="link">Latest News</Link></li>
+//         <li><Link to="/about" className="link">About</Link></li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
 import React from 'react';
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
-import '../assets/Navbar.css'; 
+import { Link } from 'react-router-dom';
+import '../assets/Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar"> 
+    <nav className="navbar">
       <h1 className="title">Fight News</h1>
-      <ul className="navLinks"> 
-        <li><Link to="/home" className="link">Home</Link></li> 
+      <ul className="navLinks">
+        <li><Link to="/home" className="link">Home</Link></li>
         <li><Link to="/news" className="link">Latest News</Link></li>
         <li><Link to="/about" className="link">About</Link></li>
       </ul>
